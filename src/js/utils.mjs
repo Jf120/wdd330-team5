5251f076583e9704ca562ea6b494da54b9d6c351
Jf120 <jafc120@gmail.com>
2023-01-21 21:48:26 -0600

    Revert "Updated branch"

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,12 +21,3 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
-
-export function getParam(param) {
-
-  // gets URL parameters
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  return urlParams.get(param);
-
-}
