187180a1ce24b03b549daf36983c281df9d74b53
Masterrasam <samuelokoosi21@gmail.com>
2023-01-27 04:30:13 +0100

    cheesy

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -30,3 +30,25 @@ export function getParam(param) {
   return urlParams.get(param);
 
 }
+
+export function renderlistwithtemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+  ) {
+    const htmlStrings=list.map(templateFn);
+    if (clear) {
+      parentElement.innerHTML - "";
+      parentElement.insertAdjacentHTML (position,HTMLsTRINGS.JOIN(""));
+    }
+     
+  }
+  export function setClick(selector, callback) {
+    qs(selector).addEvenListener("louchend", (Event) =>{
+      Event.preventDefault();
+      callback();
+    });
+    qs(selector).addEventListener("click", callback);
+  }
